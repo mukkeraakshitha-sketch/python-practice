e3b32db71366be99ebd0d5018e7045210e071742
Akshitha Mukkera <mukkeraakshitha@gmail.com>
2025-12-17 23:12:51 +0530

    Add files via upload

---
+++ basic variables.py
@@ -0,0 +1,23 @@
+a=20
+print (id(a))
+a=30
+b=40
+c=a+b
+print (c)
+c=a-b
+print (c)
+print (id(a+b))
+c=a*b
+print (c)
+c=a%b
+print (c)
+c=a/b
+print (c)
+a=50
+print (id(a))
+b=60
+print (id(b))
+a=10
+print (id(a))
+b=35
+print (id(b))
